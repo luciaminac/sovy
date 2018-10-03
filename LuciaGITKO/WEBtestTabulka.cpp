9f26f64029be08db936f23f3d3fc3ce80a5f78d3
luciaminac <lucia.minacova@gmail.com>
2018-10-03 16:29:16 +0200

    pridala som novy priecinok

---
+++ LuciaGITKO/WEBtestTabulka.cpp
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<body>
+<table style="width:50%">
+<tr>
+<td>A</td>
+<td>B</td>
+<td>C</td>
+</tr>
+<tr>
+<td rowspan="2">D</td>
+<td>E</td>
+<td colspan="2">F</td>
+</tr>
+<tr>
+<td></td>
+<td rowspan="2">H</td>
+<td>G</td>
+<td rowspan="2">K</td>
+</tr>
+<tr>
+<td>J</td>
+<td></td>
+<td bgcolor=#>I</td>
+<td></td>
+</tr>
+</tr>
+</table>
+</body>
+</html>
